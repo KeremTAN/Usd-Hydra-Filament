1abcbcb1efeda81132e844ed7929667ffb9d8710
KeremTAN <keremtan.95@hotmail.com>
2025-04-22 16:46:08 +0300

    swapChain is added to param

--- src/filRenParam.h
+++ src/filRenParam.h
@@ -4,13 +4,15 @@
 #include <pxr/imaging/hd/renderDelegate.h>
 #include <pxr/imaging/hd/renderThread.h>
 
+#include <filament/Engine.h>
+
 
 PXR_NAMESPACE_OPEN_SCOPE
-
 class FilRenParam final : public HdRenderParam {
     public:
-    FilRenParam(filament::Engine* engine, filament::Renderer* renderer, filament::Scene* scene)
-        : m_engine(engine), m_renderer(renderer), m_scene(scene)
+    FilRenParam(filament::Engine* engine, filament::Renderer* renderer,
+        filament::Scene* scene, filament::SwapChain* swapChain) :
+        m_engine(engine), m_renderer(renderer), m_scene(scene), m_swapChain(swapChain)
     {}
 
     ~FilRenParam() = default;
@@ -27,10 +29,15 @@ class FilRenParam final : public HdRenderParam {
         return m_scene;
     }
 
+    const filament::SwapChain* const GetSwapChain() const {
+        return m_swapChain;
+    }
+
 private:
     filament::Engine*   m_engine{};
     filament::Renderer* m_renderer{};
     filament::Scene*    m_scene{};
+    filament::SwapChain* m_swapChain{};
 };
 
 PXR_NAMESPACE_CLOSE_SCOPE
