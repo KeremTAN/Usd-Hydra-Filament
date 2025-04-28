18fb7f7cd554abedf4ed31abf3460447d05058aa
KeremTAN <keremtan.95@hotmail.com>
2025-04-28 17:37:55 +0300

    fiCamera Sync implemented

--- src/filCamera.cpp
+++ src/filCamera.cpp
@@ -1,11 +1,39 @@
 #include "filCamera.h"
+#include <pxr/imaging/hd/sceneDelegate.h>
 
 
 PXR_NAMESPACE_OPEN_SCOPE
 
 FilCamera::FilCamera(SdfPath const& id) : HdCamera(id) {}
 
-void FilCamera::Sync(HdSceneDelegate* sd, HdRenderParam*, HdDirtyBits*) {}
+void FilCamera::Sync(HdSceneDelegate* sceneDelegate, HdRenderParam* renderParam, HdDirtyBits* dirtyBits) {
+    HdCamera::Sync(sceneDelegate, renderParam, dirtyBits);
+
+    const VtValue& fStop { sceneDelegate->GetCameraParamValue(GetId(), HdCameraTokens->fStop) };
+
+    const VtValue& horizontalAperture { sceneDelegate->GetCameraParamValue(GetId(), HdCameraTokens->horizontalAperture) };
+
+    const VtValue& verticalAperture { sceneDelegate->GetCameraParamValue(GetId(), HdCameraTokens->verticalAperture) };
+
+    const VtValue& focusDistance { sceneDelegate->GetCameraParamValue(GetId(), HdCameraTokens->focusDistance) };
+
+    const VtValue& focalLength { sceneDelegate->GetCameraParamValue(GetId(), HdCameraTokens->focalLength) };
+
+    if (fStop.IsHolding<float>())
+        m_fStop = fStop.Get<float>();
+
+    if (horizontalAperture.IsHolding<float>())
+        m_horizontalAperture = horizontalAperture.Get<float>();
+
+    if (verticalAperture.IsHolding<float>())
+        m_verticalAperture = verticalAperture.Get<float>();
+        
+    if (focalLength.IsHolding<float>())
+        m_focalLength = focalLength.Get<float>();
+
+    if (focusDistance.IsHolding<float>())
+        m_focusDistance = focusDistance.Get<float>();
+}
 
 float FilCamera::GetFStop() const {
   return m_fStop;
