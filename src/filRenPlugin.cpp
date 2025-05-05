137b3b6f199561eb74805894b9e36a9ec180b7a6
KeremTAN <keremtan.95@hotmail.com>
2025-05-05 11:16:15 +0300

    plugInfo.json critical typo fixed & .gitignore updated

--- src/filRenPlugin.cpp
+++ src/filRenPlugin.cpp
@@ -5,6 +5,7 @@ PXR_NAMESPACE_OPEN_SCOPE
 
 TF_REGISTRY_FUNCTION(TfType) {
     HdRendererPluginRegistry::Define<FilRenPlugin>();
+    printf("[ o Filamanet Plugin ] is loaded\n");
 }
 
 FilRenPlugin::FilRenPlugin() = default;
