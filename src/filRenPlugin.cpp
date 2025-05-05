35f23e7e9b241bcaaa15cbecfd001c527326dfbb
KeremTAN <keremtan.95@hotmail.com>
2025-05-05 16:12:15 +0300

    filRenPlugin critical error is fixed in isGpuSupported.

--- src/filRenPlugin.cpp
+++ src/filRenPlugin.cpp
@@ -23,17 +23,24 @@ void FilRenPlugin::DeleteRenderDelegate(HdRenderDelegate* renderDelegate) {
     delete renderDelegate;
 }
 
-// you can think that like private access modifier
+// you can think that like a private access modifier
 namespace {
 static bool isGpuSupported() {
     using filament::Engine;
     using B = Engine::Backend;
     static const bool isSupported = []{
+
         Engine* e = Engine::create(B::METAL);
-        if (!e) e = Engine::create(B::OPENGL);
-        if (e) Engine::destroy(&e);
-        return e != nullptr;
+        if (!e) {
+            e = Engine::create(B::OPENGL);
+        }
+        if (e) {
+            Engine::destroy(&e); 
+            return true; 
+        }
+        return false;
     }();
+
     return isSupported;
 }
 } // end of namespace
